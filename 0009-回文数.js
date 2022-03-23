21d5d5fda9520b0e7f4363440088decb71c557de
纯肉攻城狮 <7547228+projectleo@user.noreply.gitee.com>
2022-03-23 22:23:00 +0800

    回文数-判断两边数字是否相等方法

--- 0009-回文数.js
+++ 0009-回文数.js
@@ -5,17 +5,29 @@
  * @param {number} x
  * @return {boolean}
  */
+// 从中间数隔开 判断两边数字是否相等
 var isPalindrome = function (x) {
     var arr = x.toString().split('')
     if (arr[0] == '-') {
         return false
     }
-    var rexarr = arr.reverse()
-    var rex = rexarr.join('')
-    if(x==rex){
-        return true
+    for (let i = 0; i < arr.length / 2; i++) {
+        if (arr[i] != arr[arr.length - i-1]) return false
     }
-    return false
+    return true
 };
-
 console.log(isPalindrome(121));
+
+// 思路：将 数字转为字符串数组，倒转字符串，让后转换为数字，对比原数与倒转数 判断是否相等
+// var isPalindrome = function (x) {
+//     var arr = x.toString().split('')
+//     if (arr[0] == '-') {
+//         return false
+//     }
+//     var rexarr = arr.reverse()
+//     var rex = rexarr.join('')
+//     if(x==rex){
+//         return true
+//     }
+//     return false
+// };
