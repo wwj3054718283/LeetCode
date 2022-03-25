fe1234c4855462d0de7bc4cf43081aa73ce31ec2
纯肉攻城狮 <7547228+projectleo@user.noreply.gitee.com>
2022-03-25 20:39:22 +0800

    阶乘后的零-在阶乘中0是2*5的结果，由于2比5少，所以只用算5的个数，但需要注意5的阶乘也可以作为0的基础，所以计算时要考虑5的阶乘

--- 0172-阶乘后的零.js
+++ 0172-阶乘后的零.js
@@ -2,36 +2,16 @@
  * 给定一个整数 n ，返回 n! 结果中尾随零的数量
  * 提示 n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1
  */
-var trailingZeroes = function (n) {
-    if (n == 0) {
-        return n
-    }
-    let total = BigInt(1)
-    let len = Math.floor(n / 2)
-    let index = 0
-    if (n % 2 != 0) {
-        total = n
-    }
-    while (index != len) {
-        total = total * BigInt((index + 1)) * BigInt((len * 2 - index))
-        index++
-    }
-    let result = 0
-    let str = total.toString()
-    let sarr = str.split('')
-    console.log(total);
-    for (let index = sarr.length - 1; index > 0; index--) {
-        // console.log(sarr[index]);
-        if (sarr[index] == 0) {
-            result++
-        } else {
-            return result
-        }
-    }
-    return result
-};
+/**
+ * n! 尾零的数量即为 n! 中因子 10 的个数，而 10=2×5，因此转换成求 n! 中质因子 2 的个数和质因子 5 的个数的较小值。
+ * 由于质因子 5 的个数不会大于质因子 2 的个数，我们可以仅考虑质因子 5 的个数。
+ * 而 n! 中质因子 5 的个数等于 [1,n]的每个数的质因子 5 的个数之和，我们可以通过遍历 [1,n][1,n] 的所有 5 的倍数求出。
+*/
 
-console.log(trailingZeroes(30));
+var trailingZeroes = function(n) {
+    return Math.floor(n/5)+Math.floor(n/25)+Math.floor(n/125)+Math.floor(n/625)+Math.floor(n/3125);
+};
+console.log(trailingZeroes(200));
 
 
 // es2020新加入 BigInt解法
@@ -64,4 +44,4 @@ console.log(trailingZeroes(30));
 //     return result
 // };
 
-// console.log(trailingZeroes(30));
+// console.log(trailingZeroes(1000));
