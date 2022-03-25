244b30dd0fa8816e38aedfd25cacd76077514485
纯肉攻城狮 <7547228+projectleo@user.noreply.gitee.com>
2022-03-25 22:46:17 +0800

    有效括号-判断方法

--- 0020-有效括号.js
+++ 0020-有效括号.js
@@ -9,5 +9,23 @@
  * @return {boolean}
  */
 var isValid = function (s) {
-
+    s = s.split('')
+    if (s[0] == ')' || s[0] == '}' || s[0] == ']') {
+        return false
+    } else {
+        while (s.length > 0) {
+            // console.log("s", s);
+            for (let i = 0; i < s.length; i++) {
+                // console.log("i:-->", i, "s[i]:-->", s[i]);
+                if (s[i] == '(' && s[i + 1] == ')' || s[i] == '[' && s[i + 1] == ']' || s[i] == '{' && s[i + 1] == '}') {
+                    s.splice(i, 2)
+                    break
+                } else if (i == s.length - 1) {
+                    return false
+                }
+            }
+            if (s.length == 0) { return true }
+        }
+    }
 };
+console.log(isValid("{[]}"));
