ec6b2690e676d823ce42283e959cad59b8ddb3be
纯肉攻城狮 <3054718283@qq.com>
2022-03-27 00:09:53 +0800

    合并两个有序链表-链表递归

---
+++ 0021-合并两个有序链表.js
@@ -0,0 +1,27 @@
+/**
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的
+ */
+
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+ var mergeTwoLists = function(l1, l2) {
+    if(l1 === null){
+        return l2;
+    }
+    if(l2 === null){
+        return l1;
+    }
+    if(l1.val < l2.val){
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    }else{
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2;
+    }
+};
+// console.log(mergeTwoLists([1, 2, 4], [1, 3, 4]));
+// console.log(mergeTwoLists([], []));
+// console.log(mergeTwoLists([], [0]));
