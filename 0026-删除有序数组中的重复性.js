376466a391e01fc3ba6e3b9d865a234389969fd7
纯肉攻城狮 <3054718283@qq.com>
2022-03-27 19:02:09 +0800

    删除有序数组中重复值--双指针方法

--- 0026-删除有序数组中的重复性.js
+++ 0026-删除有序数组中的重复性.js
@@ -1,12 +1,47 @@
 var removeDuplicates = function (nums) {
-    nums.sort()
-    for (let index = 0; index < nums.length; index++) {
-        if (nums[index] == nums[index - 1]) {
-            nums.splice(index,1)
-            index--
+    // 1.创建慢指针
+    let i = 1
+    // 2.创建快指针
+    let j = 1
+    // 3.开始循环
+    for (let index = 1; index < nums.length; index++) {
+        // 4.当慢指针指向元素值 等于 快指针 时
+        if (nums[i-1] !== nums[j]) {
+            // 4.1 慢指针 向 后移一位
+            i++
+            // 4.2 将满指针 指向元素值 改为 快指针指向元素值
+            nums[i-1] = nums[j]
+            // console.log("index:->",index,"i:-->",i);
         }
+        j++
     }
-    return nums
+    // 5.返回慢指针
+    return i
 };
-console.log(removeDuplicates([1,1,2]));
+console.log(removeDuplicates([1, 1, 2]));
 console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
+
+// var removeDuplicates = function (nums) {
+//     for (let index = 0; index < nums.length; index++) {
+//         if (nums[index] == nums[index - 1]) {
+//             nums.splice(index,1)
+//             index--
+//         }
+//     }
+//     return nums.length
+// };
+// console.log(removeDuplicates([1, 1, 2]));
+// console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
+
+// var removeDuplicates = function (nums) {
+//     nums.sort()
+//     for (let index = 0; index < nums.length; index++) {
+//         if (nums[index] == nums[index - 1]) {
+//             nums.splice(index,1)
+//             index--
+//         }
+//     }
+//     return nums
+// };
+// console.log(removeDuplicates([1,1,2]));
+// console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
