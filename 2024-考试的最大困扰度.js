486961b4c62bfd8cb8fc627a8c2451b45fc2789a
纯肉攻城狮 <3054718283@qq.com>
2022-03-29 22:51:41 +0800

    考试的最大困扰度

---
+++ 2024-考试的最大困扰度.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} answerKey
+ * @param {number} k
+ * @return {number}
+ */
+var maxConsecutiveAnswers = function (answerKey, k) {
+  return Math.max(maxTorF(answerKey, k, 'T'), maxTorF(answerKey, k, 'F'))
+};
+const maxTorF = (answerKey, k, ch) => {
+  const n = answerKey.length
+  let ans = 0
+  for (let left = 0, right = 0, sum = 0; right < n; right++) {
+    sum += answerKey[right] !== ch ? 1 : 0
+    while (sum > k) {
+      sum -= answerKey[left++] !== ch ? 1 : 0
+    }
+    ans = Math.max(ans, right - left + 1)
+  }
+  return ans
+}
+console.log(maxConsecutiveAnswers("TTFTTTTTFT", 1));
+// console.log(maxConsecutiveAnswers("TF", 2));
+// console.log(maxConsecutiveAnswers("TTFF", 2));
+// console.log(maxConsecutiveAnswers("TFFT", 1));
+// console.log(maxConsecutiveAnswers("TTFTTFTT", 1));
